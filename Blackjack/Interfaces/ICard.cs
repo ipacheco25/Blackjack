6ae28780fffee6db477aecddfa93a95d6634d7f5
ipacheco-formalloy <isaac.pacheco25@gmail.com>
2022-11-09 19:30:27 -0800

    Add XML tags to ICard.cs

--- Blackjack/Interfaces/ICard.cs
+++ Blackjack/Interfaces/ICard.cs
@@ -1,7 +1,29 @@
-﻿namespace Blackjack.Interfaces
+﻿using Blackjack.Enums;
+
+namespace Blackjack.Interfaces
 {
     internal interface ICard
     {
+        /// <summary>
+        /// The value of the Card
+        /// </summary>
+        int Value { get; }
+
+        /// <summary>
+        /// The rank of the Card
+        /// </summary>
+        ERanks Rank { get; }
+
+        /// <summary>
+        /// The suit of the Card
+        /// </summary>
+        ESuits Suit { get; }
+
+        /// <summary>
+        /// The color of the card
+        /// </summary>
+        EColors Color { get; }
+
         /// <summary>
         /// Set the value of the card to either 1 or 11 if the card is an Ace
         /// </summary>
