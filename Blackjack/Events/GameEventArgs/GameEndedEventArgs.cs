858827875037a80bb1bf3dcb4e905f8d3d62a6de
ipacheco-formalloy <isaac.pacheco25@gmail.com>
2022-11-09 19:44:31 -0800

    Remove setter and update constructor

--- Blackjack/Events/GameEventArgs/GameEndedEventArgs.cs
+++ Blackjack/Events/GameEventArgs/GameEndedEventArgs.cs
@@ -1,12 +1,13 @@
 ﻿using Blackjack.Enums;
+using Blackjack.Interfaces;
 using Blackjack.Models;
 
 namespace Blackjack.Events.GameEventArgs
 {
     internal class GameEndedEventArgs : GameEventArgs
     {
-        public EGameResults Results { get; private set; }
-        public GameEndedEventArgs(Game game, EGameResults results) : base(game)
+        public EGameResults Results { get; }
+        public GameEndedEventArgs(IGame game, EGameResults results) : base(game)
         {
             Results = results;
         }
