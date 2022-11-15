08ccfef7ceb3aa4ba1962a964042b76b16441d8f
isaac pacheco <isaac.pacheco25@gmail.com>
2022-11-15 08:04:09 -0800

    Update MainWindow

--- Blackjack/MainWindow.xaml.cs
+++ Blackjack/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blackjack.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,10 @@ namespace Blackjack
         public MainWindow()
         {
             InitializeComponent();
+            Game game = new Game();
+
+            DealerHandControl.DataContext = game.Dealer;
+            PlayerControl.DataContext = game.Player;
         }
     }
 }
