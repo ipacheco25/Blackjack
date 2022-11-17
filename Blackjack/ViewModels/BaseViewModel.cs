0a0dffbe67bebfb1719efae79b8818fb1ef5a2a6
ipacheco-formalloy <isaac.pacheco25@gmail.com>
2022-11-16 20:24:19 -0800

    Create BaseViewModel.cs

---
+++ Blackjack/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
